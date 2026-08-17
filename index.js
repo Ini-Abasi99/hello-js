2cd7e365c1a0792d574ce38f02cf394a6fda0c85
= <basseyiniabasi99@gmail.com>
2026-08-17 13:03:19 +0100

    peace

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("...")
+// Git add <file> moves (a single file) change from your working directory into the staging area( a box for packing). 
+
+
+//Git add . moves change (every file) changes from your working directory into the staging area. 
+
+
+
+
+
+console.log("Peace")
